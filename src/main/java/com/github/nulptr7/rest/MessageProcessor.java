dc2212a7416b8fe25f6a6005b8ddc8e480afed3a
Ishan Shah <ishannshah@gmail.com>
2020-04-09 17:28:59 +0530

    Fixing issues.

--- src/main/java/com/github/nulptr7/rest/MessageProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.github.nulptr7.rest;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication
-public class MessageProcessor extends ServletProcessApplication {
-}
